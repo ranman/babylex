c117de19720a0c3a8e4040e002f87770237407f3
J. Randall Hunt <randallhunt@gmail.com>
2017-01-26 03:31:33 -0800

    v 0.1 for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='babylex',
+  packages=['babylex'],
+  version='0.1',
+  description='tiny library for working with amazon lex',
+  author='Randall Hunt',
+  author_email='randallhunt@gmail.com',
+  url='https://github.com/ranman/babylex',
+  download_url='https://github.com/ranman/babylex/tarball/0.1',
+  keywords=['aws', 'chatbot'],
+  classifiers=[],
+)
